91741a3c01441b03453e94bb322d3b6d5c4a96d1
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2022-05-30 13:03:30 -0400

    Se agregó atributo alt a la imagen xd

--- src/components/sections/deptos/DeptoCard.jsx
+++ src/components/sections/deptos/DeptoCard.jsx
@@ -2,7 +2,7 @@ const DeptoCard = ({ imagen, titulo, descripcion }) => {
     return(
     <div className="ed-grid lg-grid-7 s-pxy-1 s-border s-radius s-shadow-bottom s-mb-2 s-center">
         <div className="s-mb-1 lg-cols-4 s-center">
-            <img src={imagen} className=""/>
+            <img src={imagen} alt="Imagen del departamento" />
         </div>
         <div className="lg-cols-3">
             <h3>{titulo}</h3>
